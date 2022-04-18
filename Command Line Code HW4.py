9b3257395b10af9c8ab22a52c7adb65a2c2b0d1e
gmatera99 <99137807+gmatera99@users.noreply.github.com>
2022-04-18 09:47:33 -0500

    Added URLs to the ChainerCV code base

--- Command Line Code HW4.py
+++ Command Line Code HW4.py
@@ -1,14 +1,14 @@
 # Follow installation instructions from https://chainercv.readthedocs.io/en/stable/install.html
 
-# Run in command line
+# Run in command line from the two respective folder locations with demo.py files in ChainerCV after downloading its repository
 
-# Chainer Mask RCNN: 
+# ChainerCV Mask RCNN: https://github.com/chainer/chainercv/tree/master/examples/fpn
 # Using the pretrained weight settings from a resnet that is 50 layers deep
 python demo.py --model mask_rcnn_fpn_resnet50 <image>.jpg
 # Using the pretrained weight settings from a resnet that is 101 layers deep
 python demo.py --model mask_rcnn_fpn_resnet101 <image>.jpg
 
-#Chainer Faster RCNN: 
+#ChainerCV Faster RCNN: https://github.com/chainer/chainercv/tree/master/examples/faster_rcnn
 # Default weight settings is PASCAL VOC 2007
 python demo.py <image>.jpg
 # Using the pretrained weight settings from PASCAL VOC 2007 & 2012
